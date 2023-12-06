30a4fc9c596fdc1552830c6b0e393916a546c591
Pau Pais <pau.pais@estudiants.urv.cat>
2023-12-06 08:50:23 +0100

    Day 6 (P2)

---
+++ 2023/day6_p2.py
@@ -0,0 +1,31 @@
+def main():
+    with open("inputs/day6.txt") as file:
+        lines = file.readlines()
+        time = list(map(int, lines[0].split()[1:]))
+        distance = list(map(int, lines[1].split()[1:]))  
+    time_total = ""
+    distance_total = ""
+    for time in time:
+        time_total += str(time)
+    time_total = int(time_total)
+    for distance in distance:
+        distance_total += str(distance)
+    distance_total = int(distance_total)
+    print(time_total)
+    print(distance_total)
+    local_count = 0
+    time_new_record = list()
+    how_much_ways = 0
+    
+    for i in range(time_total):
+        remaining_time = time_total
+        remaining_time -= i
+        local_count = i * remaining_time
+        if local_count > distance_total:
+            how_much_ways += 1
+        time_new_record.append(how_much_ways)
+    print(how_much_ways)
+    
+
+if __name__ == "__main__":
+    main()
